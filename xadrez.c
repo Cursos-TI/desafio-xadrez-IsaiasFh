d21de39a92b1e0ccd0b8cff6fe3a741c64b46ba1
IsaiasFh <150455844+IsaiasFh@users.noreply.github.com>
2025-09-23 00:44:43 +0000

    Desafio Novato XADREZ

--- xadrez.c
+++ xadrez.c
@@ -9,6 +9,27 @@ int main() {
             printf("Direita \n"); // Imprime a direção do movimento
         }
 
+        // Mover o Cavalo em L: 2 para baixo e 1 para a esquerda
+        printf("\n Movimento do Cavalo: \n");
+        int movimentoBaixo = 2, movimentoEsquerd = 1;
+
+        printf("\n"); // Linha branca que foi pedida
+        
+        // Primeiro, movimento para baixo com loop FOR
+        for ( int i = 0; i < movimentoBaixo; i++){
+            printf("Baixo\n");
+        }
+        // Depois, o movimento para a esquerda com loop WHILE
+            int C = 0;
+            while (C < movimentoEsquerd ){
+                printf("Esquerda \n");
+                C++;
+            }
+
+
+        
+
+
         // Mover o Bispo na Diagonal para cima e à direita
        printf("\n Movimento do Bispo: \n");
        int casasBispo = 5; // número de casas para mover
@@ -17,6 +38,7 @@ int main() {
             printf("Cima, Direita \n");// Imprime a direção do movimento
             i++;
         }
+
         // Mover a Rainha 8 casas para a esquerda
         printf("\n Movimento da Rainha: \n");
         int casasRainha = 8; // número de casas para mover
